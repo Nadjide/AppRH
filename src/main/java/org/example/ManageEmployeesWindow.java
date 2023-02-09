bc824f047472522be203a203e07e054d1fa79156
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2023-02-09 15:04:05 +0100

    manage page

--- src/main/java/org/example/ManageEmployeesWindow.java
+++ src/main/java/org/example/ManageEmployeesWindow.java
@@ -1,9 +1,14 @@
 package org.example;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class ManageEmployeesWindow extends JFrame{
 
@@ -17,22 +22,44 @@ public class ManageEmployeesWindow extends JFrame{
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        
+        // show all employees in a table
+        // add a button to delete an employee
+        // add a button to edit an employee
 
-        JPanel buttonPanel = new JPanel(new FlowLayout());
-        JButton addEmployeeButton = new JButton("Nouvel employé");
-        addEmployeeButton.addActionListener(new ActionListener() {
+        JButton showEmployeesButton = new JButton("Afficher les employés");
+        showEmployeesButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Show the add employee window
-                AddEmployeeWindow addEmployeeWindow = new AddEmployeeWindow();
-                addEmployeeWindow.setVisible(true);
+               // display all employees in a table
+
+                try{
+                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testJava", "root", "");
+                    Statement statement = connection.createStatement();
+                    ResultSet resultSet = statement.executeQuery("SELECT * FROM employees");
+
+                    // create a table to display the employees
+                    DefaultTableModel model = new DefaultTableModel();
+                    model.setColumnIdentifiers(new String[]{"ID", "Nom", "Prénom", "Email", "Salaire"});
+
+                    while(resultSet.next()){
+                        model.addRow(new Object[]{resultSet.getInt("id"), resultSet.getString("first_Name"),
+                                resultSet.getString("last_Name"), resultSet.getString("email"), resultSet.getInt("salary")});
+                    }
+
+                    //create a table to display the employees
+                    JTable table = new JTable();
+                    table.setModel(model);
+                    table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+                    table.setFillsViewportHeight(true);
+                    add(new JScrollPane(table), BorderLayout.CENTER);
+                    setVisible(true);
+                }
+                catch(Exception ex){
+                    ex.printStackTrace();
+                }
             }
         });
-
-        buttonPanel.add(addEmployeeButton);
-        JButton manageEmployeesButton = new JButton("Gestion des employés");
-
+        add(showEmployeesButton, BorderLayout.NORTH);
     }
     public static void main(String[] args) {
         new ManageEmployeesWindow();
