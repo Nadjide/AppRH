60681f399399530f8ecabe69e1c23ced15aa601a
Nadjide.O <106732960+Nadjide@users.noreply.github.com>
2023-04-04 19:44:51 +0200

    ajout tableau sur la page salaire
    ajout d'un tableau

--- src/main/java/org/example/SalaryWindow.java
+++ src/main/java/org/example/SalaryWindow.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,16 +13,24 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class SalaryWindow extends JFrame {
+    private JTable table;
+    private JTextField searchField;
+    private JButton searchButton;
+    private DefaultTableModel tableModel;
+
     public SalaryWindow() {
         setTitle("Gestion des salaires");
-        setSize(600, 400);
+        setSize(800, 600);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        double medanSalary = calculateMedianSalary();
-        JLabel medianSalaryLabel = new JLabel("Le salaire médian est de " + medanSalary + "€");
+        double medianSalary = calculateMedianSalary();
+        JLabel medianSalaryLabel = new JLabel("Le salaire médian est de " + medianSalary + "€");
         add(medianSalaryLabel, BorderLayout.NORTH);
+
+        createTable();
+        createSearchPanel();
 
         setVisible(true);
 
@@ -39,11 +48,80 @@ public class SalaryWindow extends JFrame {
         });
         buttonPanel.add(returnButton);
         add(buttonPanel, BorderLayout.SOUTH);
+    }
 
+    private void createTable() {
+        String[] columnNames = {"ID", "Prénom", "Nom", "Email", "Adresse", "Téléphone", "Date de naissance", "Date d'embauche", "Salaire"};
+        tableModel = new DefaultTableModel(columnNames, 0);
+        table = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(table);
+        add(scrollPane, BorderLayout.CENTER);
+        loadEmployees();
+    }
+
+    private void createSearchPanel() {
+        JPanel searchPanel = new JPanel(new FlowLayout());
+        searchField = new JTextField(20);
+        searchButton = new JButton("Rechercher");
+        searchButton.addActionListener(e -> searchEmployees());
+        searchPanel.add(searchField);
+        searchPanel.add(searchButton);
+        add(searchPanel, BorderLayout.NORTH);
+    }
+
+    private void loadEmployees() {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testjava", "root", "");
+            Statement statement = connection.createStatement();
+            ResultSet employees = statement.executeQuery("SELECT * FROM employees");
+            while (employees.next()) {
+                int id = employees.getInt("id");
+                String firstName = employees.getString("first_name");
+                String lastName = employees.getString("last_name");
+                String email = employees.getString("email");
+                String address = employees.getString("address");
+                String phone = employees.getString("phone");
+                String birthDate = employees.getString("birth_date");
+                String hireDate = employees.getString("hire_date");
+                double salary = employees.getDouble("salary");
+                Object[] rowData = {id, firstName, lastName, email, address, phone, birthDate, hireDate, salary};
+                tableModel.addRow(rowData);
+            }
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    private void searchEmployees() {
+        // Clear the table
+        tableModel.setRowCount(0);
+
+        String searchQuery = searchField.getText().trim();
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testjava", "root", "");
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM employees WHERE first_name LIKE '%" + searchQuery + "%' OR last_name LIKE '%" + searchQuery + "%' OR email LIKE '%" + searchQuery + "%' OR address LIKE '%" + searchQuery + "%' OR phone LIKE '%"+ searchQuery + "%'";
+            ResultSet employees = statement.executeQuery(query);
+            while (employees.next()) {
+                int id = employees.getInt("id");
+                String firstName = employees.getString("first_name");
+                String lastName = employees.getString("last_name");
+                String email = employees.getString("email");
+                String address = employees.getString("address");
+                String phone = employees.getString("phone");
+                String birthDate = employees.getString("birth_date");
+                String hireDate = employees.getString("hire_date");
+                double salary = employees.getDouble("salary");
+                Object[] rowData = {id, firstName, lastName, email, address, phone, birthDate, hireDate, salary};
+                tableModel.addRow(rowData);
+            }
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
     }
 
     private double calculateMedianSalary() {
-
         ArrayList<Double> salaries = new ArrayList<Double>();
 
         try {
@@ -68,12 +146,9 @@ public class SalaryWindow extends JFrame {
         } else {
             return (salaries.get(size / 2 - 1) + salaries.get(size / 2)) / 2;
         }
-
     }
 
-        public static void main (String[]args){
-            new SalaryWindow();
-        }
+    public static void main(String[] args) {
+        new SalaryWindow();
     }
-
-
+}
